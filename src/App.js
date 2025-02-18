ca732fe001c2a6901214cf0ec7773db922419bc2
Dominic Prestia <dominicprestia@gmail.com>
2025-02-18 15:31:55 -0600

    Added a notVisible function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Xbutton from "./componenets/Button/Xbutton";
 
 function App() {
+
+  const [closed, setClosed] = useState(false);
+
+  const handleClosed = () => {
+    setClosed(!closed);
+  }
+
   return (
     <div className="App">
 
       <div className="TopBoundingBox">
-        <div className="BotBoundingBox">
+        <div className={closed ? "notVisible" : "BotBoundingBox"} onClick={handleClosed} >
           <p>Custom 1 Button</p>
           <Xbutton variant="primary">Custom Button</Xbutton>
         </div>
