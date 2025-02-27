a4fe8f2d6b966fe0c82c663436c7e5014c44d244
Dominic Prestia <dominicprestia@gmail.com>
2025-02-27 17:08:24 -0600

    Trying to get calendar selection to work.

--- src/componenets/Calendar/BodyCalendar.jsx
+++ src/componenets/Calendar/BodyCalendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, } from 'react'
 import style from './bodyCalendar.module.css'
 import Day from './Day';
 
@@ -7,17 +7,26 @@ const BodyCalendar = ({ month, year }) => {
     const calendarDimensions = Array.from({ length: 6 }, (value, i) => Array.from({ length: 7 }, (_, i2) => i2 + 1))
     const firstDay = new Date(year, month, 1);
     const lastDay = new Date(year, month + 1, 0);
+    // const [isHovered,setIsHovered] = useState({});
+    const [calendarValues, setCalendarValues] = useState([])
     const pickRange = 7;
+    let pos = 0;
 
-    const calculateDay = (startDay, doyOfWeek, endDay) => {
+    const calculateDay = (startDay, doyOfWeek, position, endDay) => {
         const firstDay = startDay;
         const lastDay = endDay;
         const i = doyOfWeek;
 
+
+
         let content;
 
         if (firstDay.getDay() === i && firstDay.getMonth() <= lastDay.getMonth()) {
-            content = firstDay.getDate()
+            content = {
+                id: position,
+                day: firstDay.getDate(),
+                isHovered: false,
+            }
 
             firstDay.setDate(firstDay.getDate() + 1);
         }
@@ -27,31 +36,57 @@ const BodyCalendar = ({ month, year }) => {
 
             prevMonthDay.setDate(firstDay.getDate() - minusDays);
 
-            content = prevMonthDay.getDate()
+            content = {
+                id: position,
+                day: prevMonthDay.getDate(),
+                isHovered: false,
+            }
         }
         else {
-            content = firstDay.getDate()
+            content = {
+                id: position,
+                day: firstDay.getDate(),
+                isHovered: false,
+            }
             firstDay.setDate(firstDay.getDate() + 1)
         }
         return content;
     }
 
-    const handleMouseOver = (y,x) => {
-        console.log("Mouse Over",y, ", ", x)
+    const handleMouseOver = (currentPos, calendarArray) => {
+        console.log(currentPos);
+        currentPos.isHovered = 5;
     }
 
-    console.log("RENDER")
+    // useEffect(() => {
+    //     setCalendarValues(calendarDimensions)
+    // }, [])
+
+
+
+
+
+    // console.log("RENDER", calendarValues)
 
     return (
         <div className={style.topContainer}>
-            {calendarDimensions.map((dayOfWeek, y) => {
+            {calendarDimensions.map((row, y) => {
                 return (
                     <div key={y} className={style.column}>
-                        {dayOfWeek.map((day, x) => {
-                            
+                        {row.map((col, x) => {
+                            pos++
+                            //Should exectued calculateDay here for the value of day and pass it down. You should set the value of the location in the array to the day.
+                            // each value can be an object: {day:x, hovered:true/false, inCurrentMonth: true/false}
+                            calendarDimensions[y][x] = calculateDay(firstDay, x, pos, lastDay)
+
+                            const calValue = calendarDimensions[y][x];
+
+                            // console.log("Calendar Value: ", calValue);
+                            // console.log("Calendar Array: ", calendarDimensions)
+
                             return (
-                                <div onMouseOver={()=>handleMouseOver(y, x)}  key={x} className={style.row}>
-                                    <Day day={calculateDay(firstDay,x,lastDay)}/>
+                                <div onMouseOver={() => handleMouseOver(calValue, calendarDimensions)} key={x} className={`${style.row} ${calValue.isHovered ? style.active : ""}`}>
+                                    <Day day={calValue.day} />
                                 </div>
                             )
                         })}
